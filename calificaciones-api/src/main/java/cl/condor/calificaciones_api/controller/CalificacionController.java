94e81f7fbb9130cb8050ad29c75b771df6460c54
logicaltomy <logicaltomy@gmail.com>
2025-10-08 00:27:46 -0300

    1.9.0 - postman con db probados

--- calificaciones-api/src/main/java/cl/condor/calificaciones_api/controller/CalificacionController.java
+++ calificaciones-api/src/main/java/cl/condor/calificaciones_api/controller/CalificacionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+// get y post funcionan
 @RequestMapping("/api/v1/calificaciones")
 public class CalificacionController {
 
